761616223ff15c4d55b490318006aa2c91b36d68
estevam98 <jhonatanm222@gmail.com>
2017-04-22 14:14:25 -0300

    Implementado explorer

--- src/main/java/br/univel/ChatRedes/view/Chat.java
+++ src/main/java/br/univel/ChatRedes/view/Chat.java
@@ -22,35 +22,35 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-public class Chat extends JFrame{
+public class Chat extends JFrame {
 
 	private JTextField textFieldMensagem;
-	
+
 	private static JFrame frame;
 
 	public Chat() {
-		
+
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 			SwingUtilities.updateComponentTreeUI(this);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		setTitle("Chat");
 		setBounds(100, 100, 511, 370);
 		setLocationRelativeTo(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		getContentPane().add(panel, BorderLayout.CENTER);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{59, 326, 31, 17, 32, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[] { 59, 326, 31, 17, 32, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0, 0 };
+		gbl_panel.columnWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
-		
+
 		JLabel lblUser = new JLabel("user");
 		lblUser.setFont(new Font("Yu Gothic UI", Font.PLAIN, 15));
 		GridBagConstraints gbc_lblUser = new GridBagConstraints();
@@ -59,7 +59,7 @@ public class Chat extends JFrame{
 		gbc_lblUser.gridx = 1;
 		gbc_lblUser.gridy = 0;
 		panel.add(lblUser, gbc_lblUser);
-		
+
 		JPanel panel_1 = new JPanel();
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
 		gbc_panel_1.gridwidth = 3;
@@ -69,26 +69,26 @@ public class Chat extends JFrame{
 		gbc_panel_1.gridy = 1;
 		panel.add(panel_1, gbc_panel_1);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{0, 0};
-		gbl_panel_1.rowHeights = new int[]{0, 0};
-		gbl_panel_1.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel_1.columnWidths = new int[] { 0, 0 };
+		gbl_panel_1.rowHeights = new int[] { 0, 0 };
+		gbl_panel_1.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panel_1.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
 		panel_1.setLayout(gbl_panel_1);
-		
+
 		JTextArea textArea = new JTextArea();
 		textArea.setEditable(false);
-		GridBagConstraints gbc_textArea = new GridBagConstraints(); 
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
 		gbc_textArea.fill = GridBagConstraints.BOTH;
 		gbc_textArea.gridx = 0;
 		gbc_textArea.gridy = 0;
 		panel_1.add(textArea, gbc_textArea);
-		
+
 		textFieldMensagem = new JTextField();
 		textFieldMensagem.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 13));
 		textFieldMensagem.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				if(e.getKeyCode() == KeyEvent.VK_ENTER){
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 					textArea.append(textFieldMensagem.getText() + "\n");
 					textFieldMensagem.setText("");
 				}
@@ -101,11 +101,11 @@ public class Chat extends JFrame{
 		gbc_textFieldMensagem.gridy = 2;
 		panel.add(textFieldMensagem, gbc_textFieldMensagem);
 		textFieldMensagem.setColumns(10);
-		
+
 		Icon imagem_mensagem = new ImageIcon("send-button.png");
 		JButton btnEnviar = new JButton(imagem_mensagem);
 		btnEnviar.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				textArea.append(textFieldMensagem.getText() + " \n");
@@ -119,19 +119,14 @@ public class Chat extends JFrame{
 		gbc_btnNewButton.gridx = 2;
 		gbc_btnNewButton.gridy = 2;
 		panel.add(btnEnviar, gbc_btnNewButton);
-		
-		
+
 		Icon imagem_arquivo = new ImageIcon("send-arquivo.png");
 		JButton btnArquivo = new JButton(imagem_arquivo);
 		btnArquivo.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				try {
-					Runtime.getRuntime().exec("explorer.exe " + "C:\\Usuario\\");
-				} catch (Exception e2) {
-					e2.printStackTrace();
-				}
+				new Explorer().buscar();
 			}
 		});
 		btnArquivo.setContentAreaFilled(false);
@@ -141,12 +136,12 @@ public class Chat extends JFrame{
 		gbc_btnArquivo.gridx = 3;
 		gbc_btnArquivo.gridy = 2;
 		panel.add(btnArquivo, gbc_btnArquivo);
-	
+
 		frame = this;
 	}
-	
-	public static JFrame getFrame(){
+
+	public static JFrame getFrame() {
 		return frame;
 	}
-}
 
+}
---
+++ src/main/java/br/univel/ChatRedes/view/Explorer.java
@@ -0,0 +1,42 @@
+package br.univel.ChatRedes.view;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class Explorer extends JFrame {
+
+	public void buscar() {
+		FileNameExtensionFilter fileNameExtensionFilter = new FileNameExtensionFilter("png", "txt", "doc", "jpg");
+		
+		JFileChooser fileChooser = new JFileChooser();
+		
+		fileChooser.setFileFilter(fileNameExtensionFilter);
+		fileChooser.setDialogTitle("Selecionando arquivo");
+		
+		int resposta = fileChooser.showOpenDialog(null);
+		
+		if (resposta == JFileChooser.APPROVE_OPTION) {
+			File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+			FileReader fileReader;
+			
+			try {
+				fileReader = new FileReader(file);
+				BufferedReader buffered = new BufferedReader(fileReader);
+				
+				while (buffered.ready()) {
+					System.out.println(buffered.readLine() + " \n");
+				}
+				
+				buffered.close();
+				fileReader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
